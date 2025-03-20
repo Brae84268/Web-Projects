186f2e76aca68d744e4221bc56d842a7a784ad61
Brae84268 <kjh84268@gmail.com>
2025-03-20 11:46:52 +0900

    자바스크립트 학습 시작

---
+++ 07/index.js
@@ -0,0 +1,10 @@
+let age = 25;
+let userName = "Dong Hyun" + "Kim";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = {
+  title: "Developer",
+  place: "Seoul",
+  salery: 9500000,
+};
+
+alert(hobbies[0]);

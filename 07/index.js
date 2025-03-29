212dd26308659c5c3ff4cc64bec10b9207b45247
Brae84268 <kjh84268@gmail.com>
2025-03-30 01:05:56 +0900

    DOM 연습

--- 07/index.js
+++ 07/index.js
@@ -1,3 +1,15 @@
 //document.body.children[1].children[0].href = "https://google.com";
 
-document.getElementById("external-link");
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
+
+anchorElement = document.querySelector("#external-link");
+anchorElement.href = "https://naver.com";
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "Go Google";
+
+let firstParagraph = document.querySelector("p");
+
+firstParagraph.append(newAnchorElement);

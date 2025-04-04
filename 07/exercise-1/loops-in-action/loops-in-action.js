de9454eca29b0955e7281d6923c5e2a858fb7fc8
Brae84268 <kjh84268@gmail.com>
2025-04-04 19:27:19 +0900

    자바스크립트 기초 연습

--- 07/exercise-1/loops-in-action/loops-in-action.js
+++ 07/exercise-1/loops-in-action/loops-in-action.js
@@ -29,3 +29,64 @@ function highlightLinks() {
 }
 
 highlightLinksBtnElement.addEventListener("click", highlightLinks);
+
+const dummyUserData = {
+  firstName: "Kim",
+  lastName: "Brae",
+  age: 25,
+};
+
+const displayUserDataBtnElement = document.querySelector("#user-data button");
+
+function displayUserData() {
+  const outputDataElement = document.getElementById("output-user-data");
+
+  outputDataElement.innerHTML = "";
+
+  for (const userData in dummyUserData) {
+    const newUserDataLiElement = document.createElement("li");
+    const outputText = userData.toUpperCase() + ": " + dummyUserData[userData];
+    newUserDataLiElement.textContent = outputText;
+    outputDataElement.append(newUserDataLiElement);
+  }
+}
+
+displayUserDataBtnElement.addEventListener("click", displayUserData);
+
+const rollDiceBtnElement = document.querySelector("#statistics button");
+
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function deriveNumOfDiceRolls() {
+  const targetNumInputElement = document.getElementById("user-target-number");
+  const diceRollsLiElement = document.getElementById("dice-rolls");
+
+  const enterdNum = targetNumInputElement.value;
+  diceRollsLiElement.innerHTML = " ";
+
+  let hasRolledTargetNum = false;
+  let numOfRolls = 0;
+
+  while (!hasRolledTargetNum) {
+    const rolledNum = rollDice();
+    // if (rolledNum == enterdNum) {
+    //   hasRolledTargetNum = true;
+    // }
+    numOfRolls++;
+    const newRollLiElement = document.createElement("li");
+    const outputText = "Roll " + numOfRolls + ": " + rolledNum;
+    newRollLiElement.textContent = outputText;
+    diceRollsLiElement.append(newRollLiElement);
+    hasRolledTargetNum = rolledNum == enterdNum;
+  }
+
+  const outputTotalRollsElement = document.getElementById("output-total-rolls");
+  const outputTargetNumElement = document.getElementById("output-target-number");
+
+  outputTargetNumElement.textContent = enterdNum;
+  outputTotalRollsElement.textContent = numOfRolls;
+}
+
+rollDiceBtnElement.addEventListener("click", deriveNumOfDiceRolls);
